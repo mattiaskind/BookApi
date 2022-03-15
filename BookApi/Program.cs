5b3cc27fa84991d20603dbbecf0b858b485dd929
Mattias Kind <mattias.kind@gmail.com>
2022-03-15 11:46:10 +0100

    Kommentarer

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -4,7 +4,7 @@ using BookApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Registrerar databasklassen som en dependency, baserat på interfacet databasklassen implementerar
-// Singleton så att listan i BooksDb är tillgänglig under tiden servern körs
+// Singleton - så att en och samma instans av BooksDb är tillgänglig under tiden servern körs
 builder.Services.AddSingleton<IBooksDb, BooksDb>();
 
 builder.Services.AddControllers();
@@ -13,6 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // För att hindra ett fel som uppstår när bok skapas då dotnet-ramverket tar bort ändelsen "async" i metodnamnen vid körning
+// Jag har försökt utgå från async/await även om mina objekt i det här fallet bara sparas i en List.
 builder.Services.AddMvc(options => options.SuppressAsyncSuffixInActionNames = false);
 
 var corsPolicy = "corsPolicy";
@@ -39,14 +40,13 @@ if (app.Environment.IsDevelopment())
 }
 
 /* Cors-policyn tillåter för tillfället anrop från alla ursprung för
- * att göra det enklare att testa api:t fullt ut. Skulle behöva ses
- * över vid publicering
+ * att göra det enklare att testa api:t fullt ut. 
 */
 app.UseCors(corsPolicy);
 
-/* Vid körning av projektet BookApi (client) används
- * de statiska filerna i wwwroot som ger tillgång till ett
- * gränsnitt som interagerar med api:t
+/* Vid körning av projektet BookApi (starta med frontend) används
+ * de statiska filerna i wwwroot, vilket bland annat består av javascript som jag skrivit
+ * för att få ett gränsnitt som kan interagera med api:t
  */
 app.UseDefaultFiles();
 app.UseStaticFiles();
